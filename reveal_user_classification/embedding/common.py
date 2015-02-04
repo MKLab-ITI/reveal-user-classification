Feature normalization fix.

reveal_user_classification/embedding/common.py
@@ -18,17 +18,16 @@ def normalize_community_features(features):
     # Calculate inverse document frequency.
     features = features.tocsc()
     for j in range(features.shape[1]):
-        document_frequency = features.getcol(j).data
-        if document_frequency is not None:
-            if document_frequency.size > 1:
-                features.data[features.indptr[j]: features.indptr[j + 1]] =\
-                    features.data[features.indptr[j]: features.indptr[j + 1]]/np.sqrt(np.log(document_frequency.size))
+        document_frequency = features.getcol(j).data.size
+        if document_frequency > 1:
+            features.data[features.indptr[j]: features.indptr[j + 1]] =\
+                features.data[features.indptr[j]: features.indptr[j + 1]]/np.sqrt(np.log(document_frequency))
 
     # Normalize each row of term frequencies to 1
     features = features.tocsr()
     for i in range(features.shape[0]):
         term_frequency = features.getrow(i).data
-        if term_frequency is not None:
+        if term_frequency.size > 0:
             features.data[features.indptr[i]: features.indptr[i + 1]] =\
                 features.data[features.indptr[i]: features.indptr[i + 1]]/np.sqrt(np.sum(np.power(term_frequency, 2)))
 