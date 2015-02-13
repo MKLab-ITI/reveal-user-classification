Added cython profiling flag + rw fixes + feature normalization options.

reveal_user_classification/eps_randomwalk/similarity.py
@@ -9,7 +9,9 @@ from reveal_user_classification.eps_randomwalk.push import pagerank_lazy_push
 from reveal_user_classification.eps_randomwalk.push import regularized_limit_commute
 
 
-def fast_approximate_personalized_pagerank(w_i,
+def fast_approximate_personalized_pagerank(s,
+                                           r,
+                                           w_i,
                                            a_i,
                                            out_degree,
                                            in_degree,
@@ -22,8 +24,8 @@ def fast_approximate_personalized_pagerank(w_i,
     number_of_nodes = a_i.size
 
     # Initialize approximate PageRank and residual distributions
-    s = np.zeros(number_of_nodes, dtype=np.float64)
-    r = np.zeros(number_of_nodes, dtype=np.float64)
+    # s = np.zeros(number_of_nodes, dtype=np.float64)
+    # r = np.zeros(number_of_nodes, dtype=np.float64)
     r[seed_node] = 1.0
 
     # Initialize queue of nodes to be pushed
@@ -62,13 +64,15 @@ def fast_approximate_personalized_pagerank(w_i,
                 pushable.extend(a_i[push_node][i])
 
     # Sparsify and return.
-    s_sparse = sparse.csr_matrix(s, shape=(1, number_of_nodes))
-    r_sparse = sparse.csr_matrix(r, shape=(1, number_of_nodes))
+    # s_sparse = sparse.csr_matrix(s, shape=(1, number_of_nodes))
+    # r_sparse = sparse.csr_matrix(r, shape=(1, number_of_nodes))
 
-    return s_sparse, r_sparse, number_of_push_operations
+    return number_of_push_operations
 
 
-def lazy_approximate_personalized_pagerank(w_i,
+def lazy_approximate_personalized_pagerank(s,
+                                           r,
+                                           w_i,
                                            a_i,
                                            out_degree,
                                            in_degree,
@@ -86,8 +90,8 @@ def lazy_approximate_personalized_pagerank(w_i,
     number_of_nodes = a_i.size
 
     # Initialize approximate PageRank and residual distributions
-    s = np.zeros(number_of_nodes, dtype=np.float64)
-    r = np.zeros(number_of_nodes, dtype=np.float64)
+    # s = np.zeros(number_of_nodes, dtype=np.float64)
+    # r = np.zeros(number_of_nodes, dtype=np.float64)
     r[seed_node] = 1.0
 
     # Initialize queue of nodes to be pushed
@@ -149,13 +153,15 @@ def lazy_approximate_personalized_pagerank(w_i,
             number_of_push_operations += 1
 
     # Sparsify and return.
-    s_sparse = sparse.csr_matrix(s, shape=(1, number_of_nodes))
-    r_sparse = sparse.csr_matrix(r, shape=(1, number_of_nodes))
+    # s_sparse = sparse.csr_matrix(s, shape=(1, number_of_nodes))
+    # r_sparse = sparse.csr_matrix(r, shape=(1, number_of_nodes))
 
-    return s_sparse, r_sparse, number_of_push_operations
+    return number_of_push_operations
 
 
-def fast_approximate_regularized_commute(w_i,
+def fast_approximate_regularized_commute(s,
+                                         r,
+                                         w_i,
                                          a_i,
                                          out_degree,
                                          in_degree,
@@ -180,8 +186,8 @@ def fast_approximate_regularized_commute(w_i,
     number_of_nodes = a_i.size
 
     # Initialize the similarity matrix slice and the residual distribution
-    s = np.zeros(number_of_nodes, dtype=np.float64)
-    r = np.zeros(number_of_nodes, dtype=np.float64)
+    # s = np.zeros(number_of_nodes, dtype=np.float64)
+    # r = np.zeros(number_of_nodes, dtype=np.float64)
     s[seed_node] = 1.0
     r[seed_node] = 1.0
 
@@ -225,7 +231,7 @@ def fast_approximate_regularized_commute(w_i,
                 pushable.extend(a_i[push_node][i])
 
     # Sparsify and return.
-    s_sparse = sparse.csr_matrix(s, shape=(1, number_of_nodes))
-    r_sparse = sparse.csr_matrix(r, shape=(1, number_of_nodes))
+    # s_sparse = sparse.csr_matrix(s, shape=(1, number_of_nodes))
+    # r_sparse = sparse.csr_matrix(r, shape=(1, number_of_nodes))
 
-    return s_sparse, r_sparse, number_of_push_operations
+    return number_of_push_operations

reveal_user_classification/eps_randomwalk/transition.py
@@ -4,6 +4,40 @@ import numpy as np
 import scipy.sparse as sparse
 
 
+def get_label_based_random_walk_matrix(adjacency_matrix, labelled_nodes, label_absorption_probability):
+    """
+    Returns the label-absorbing random walk transition probability matrix.
+
+    Input:  - A: A sparse matrix that contains the adjacency matrix of the graph.
+
+    Output: - W: A sparse matrix that contains the natural random walk transition probability matrix.
+    """
+    # Turn to sparse.csr_matrix format for faster row access.
+    rw_transition = sparse.csr_matrix(adjacency_matrix, dtype=np.float64)
+
+    # Sum along the two axes to get out-degree and in-degree, respectively
+    out_degree = rw_transition.sum(axis=1)
+    in_degree = rw_transition.sum(axis=0)
+
+    # Form the inverse of the diagonal matrix containing the out-degree
+    for i in np.arange(rw_transition.shape[0]):
+        rw_transition.data[rw_transition.indptr[i]: rw_transition.indptr[i + 1]] =\
+            rw_transition.data[rw_transition.indptr[i]: rw_transition.indptr[i + 1]]/out_degree[i]
+
+    out_degree = np.array(out_degree).astype(np.float64).reshape(out_degree.size)
+    in_degree = np.array(in_degree).astype(np.float64).reshape(in_degree.size)
+
+    # When the random walk agent encounters a labelled node, there is a probability that it will be absorbed.
+    diag = np.zeros_like(out_degree)
+    diag[labelled_nodes] = 1.0
+    diag = sparse.dia_matrix((diag, [0]), shape=(in_degree.size, in_degree.size))
+    diag = sparse.csr_matrix(diag)
+
+    rw_transition[labelled_nodes, :] = (1-label_absorption_probability)*rw_transition[labelled_nodes, :] + label_absorption_probability*diag[labelled_nodes, :]
+
+    return rw_transition, out_degree, in_degree
+
+
 def get_natural_random_walk_matrix(adjacency_matrix):
     """
     Returns the natural random walk transition probability matrix given the adjacency matrix.
@@ -13,7 +47,7 @@ def get_natural_random_walk_matrix(adjacency_matrix):
     Output: - W: A sparse matrix that contains the natural random walk transition probability matrix.
     """
     # Turn to sparse.csr_matrix format for faster row access.
-    rw_transition = sparse.csr_matrix(adjacency_matrix, dtype=np.float64)
+    rw_transition = sparse.csr_matrix(adjacency_matrix, dtype=np.float64, copy=True)
 
     # Sum along the two axes to get out-degree and in-degree, respectively
     out_degree = rw_transition.sum(axis=1)

reveal_user_classification/experiments/holdout.py
@@ -33,7 +33,8 @@ def get_folds_generator(node_label_matrix, labelled_node_indices, number_of_cate
             for trial in np.arange(30):
                 train, test = valid_train_test(node_label_matrix[labelled_node_indices, :],
                                                training_set_size,
-                                               number_of_categories)
+                                               number_of_categories,
+                                               trial)
                 train = labelled_node_indices[train]
                 test = labelled_node_indices[test]
 
@@ -92,7 +93,8 @@ def generate_folds(node_label_matrix, labelled_node_indices, number_of_categorie
     for trial in np.arange(30):
         train, test = valid_train_test(node_label_matrix[labelled_node_indices, :],
                                        training_set_size,
-                                       number_of_categories)
+                                       number_of_categories,
+                                       trial)
         train = labelled_node_indices[train]
         test = labelled_node_indices[test]
 
@@ -103,13 +105,14 @@ def generate_folds(node_label_matrix, labelled_node_indices, number_of_categorie
     return folds
 
 
-def valid_train_test(node_label_matrix, training_set_size, number_of_categories):
+def valid_train_test(node_label_matrix, training_set_size, number_of_categories, random_seed=0):
     """
     Partitions the labelled node set into training and testing set, making sure one category exists in both sets.
 
     Inputs:  - node_label_matrix: The node-label ground truth in a SciPy sparse matrix format.
              - training_set_size: The minimum required size for the training set.
              - number_of_categories: The number of categories/classes in the experiments.
+             - random_seed: A seed for numpy random.
 
     Outputs: - train_set: A NumPy array containing the training set node ids.
              - test_set: A NumPy array containing the testing set node ids.
@@ -119,6 +122,7 @@ def valid_train_test(node_label_matrix, training_set_size, number_of_categories)
     number_of_labelled_nodes = node_label_matrix.shape[0]
 
     # Randomize process
+    np.random.seed(random_seed)
     perm = rand.permutation(number_of_labelled_nodes)
     node_label_matrix = node_label_matrix[perm, :]
 