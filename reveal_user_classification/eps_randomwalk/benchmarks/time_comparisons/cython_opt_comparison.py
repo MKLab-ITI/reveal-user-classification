93c62d7135ee0b4a3c3f3a5170d6836f8ff974ec
georgiosrizos <georgevrizos@gmail.com>
2015-06-26 16:01:43 +0300

    Update approximate vertex-centric similarity calculation files.

--- reveal_user_classification/eps_randomwalk/benchmarks/time_comparisons/cython_opt_comparison.py
+++ reveal_user_classification/eps_randomwalk/benchmarks/time_comparisons/cython_opt_comparison.py
@@ -1,8 +1,5 @@
 __author__ = 'Georgios Rizos (georgerizos@iti.gr)'
 
-import numpy as np
-
-from reveal_user_classification.common.config_package import get_raw_datasets_path
 from reveal_user_classification.datautil.asu_read_data import read_adjacency_matrix
 from reveal_user_classification.eps_randomwalk.benchmarks.benchmark_design import cython_optimization_benchmark
 
@@ -26,37 +23,16 @@ NUMBER_OF_TRIALS = 10
 # Read data.
 ########################################################################################################################
 # Define data path.
-EDGE_LIST_PATH = get_raw_datasets_path() + "/ASU/" + DATASET + "/adjacency_matrices/edges.csv"
+EDGE_LIST_PATH = get_raw_datasets_path() + "/ASU/" + DATASET + "/edges.csv"
 adjacency_matrix = read_adjacency_matrix(EDGE_LIST_PATH, ',')
 number_of_nodes = adjacency_matrix.shape[0]
 
 ########################################################################################################################
 # Perform experiment.
 ########################################################################################################################
-# Initialize result arrays.
-total_execution_time_non_opt = np.zeros(number_of_nodes, dtype=np.float64)
-total_execution_time_opt = np.zeros(number_of_nodes, dtype=np.float64)
-
-# Run trials.
-for trial in np.arange(NUMBER_OF_TRIALS):
-    execution_time_non_opt,\
-    execution_time_opt, = cython_optimization_benchmark(adjacency_matrix,
-                                                        ALPHA,
-                                                        EPSILON)
-
-    total_execution_time_non_opt += execution_time_non_opt
-
-    total_execution_time_opt += execution_time_opt
-
-########################################################################################################################
-# Print results.
-########################################################################################################################
-# Print python version results.
-print("Python version:")
-print("Total time: ", np.sum(total_execution_time_non_opt)/NUMBER_OF_TRIALS)
-print(" Mean time: ", np.mean(total_execution_time_non_opt)/NUMBER_OF_TRIALS)
-
-# Print cython-optimized version results.
-print("Cython version:")
-print("Total time: ", np.sum(total_execution_time_opt)/NUMBER_OF_TRIALS)
-print(" Mean time: ", np.mean(total_execution_time_opt)/NUMBER_OF_TRIALS)
+profiler_stats = cython_optimization_benchmark(NUMBER_OF_TRIALS,
+                                               adjacency_matrix,
+                                               ALPHA,
+                                               EPSILON,
+                                               LAZINESS,
+                                               profile_stat_folder)
--- reveal_user_classification/eps_randomwalk/benchmarks/time_comparisons/eps_rw_comparison.py
+++ reveal_user_classification/eps_randomwalk/benchmarks/time_comparisons/eps_rw_comparison.py
@@ -1,89 +1,54 @@
 __author__ = 'Georgios Rizos (georgerizos@iti.gr)'
 
-import numpy as np
-
-from reveal_user_classification.common.config_package import get_raw_datasets_path
 from reveal_user_classification.datautil.asu_read_data import read_adjacency_matrix
-from reveal_user_classification.eps_randomwalk.benchmarks.benchmark_design import similarity_slice_benchmark
+# from reveal_user_classification.datautil.snow_datautil.snow_read_data import read_adjacency_matrix
+# from reveal_user_classification.datautil.insight_datautil.insight_read_data import read_adjacency_matrix
+from reveal_user_classification.eps_randomwalk.benchmarks.benchmark_design import similarity_slice_benchmark,\
+    aggregate_measurements, print_stats
 
 
 ########################################################################################################################
 # Configure experiment.
 ########################################################################################################################
 # Select dataset
-DATASET = "BlogCatalog"  # Choices are: BlogCatalog, Flickr, YouTube.
+DATASET = "YouTube_cc"  # Choices are: BlogCatalog, Flickr, YouTube_cc.
 
 # Define approximate PageRank method parameters.
-ALPHA = 0.1
+RHO_EFFECTIVE = 0.1
 EPSILON = 0.00001
 
 # Define laziness factor for the lazy PageRank.
 LAZINESS = 0.5
 
-NUMBER_OF_TRIALS = 10
+NUMBER_OF_TRIALS = 2
 
 ########################################################################################################################
 # Read data.
 ########################################################################################################################
 # Define data path.
-EDGE_LIST_PATH = get_raw_datasets_path() + "/ASU/" + DATASET + "/adjacency_matrices/edges.csv"
+EDGE_LIST_PATH = get_raw_datasets_path() + "/ASU/" + DATASET + "/edges.csv"
 adjacency_matrix = read_adjacency_matrix(EDGE_LIST_PATH, ',')
 number_of_nodes = adjacency_matrix.shape[0]
+
+profile_individual_stat_folder = get_memory_path() + "/ASU/" + DATASET + "/profile/individual"
+profile_aggregate_stat_folder = get_memory_path() + "/ASU/" + DATASET + "/profile/aggregate"
+profile_output_stat_folder = get_memory_path() + "/ASU/" + DATASET + "/profile/output"
 
 ########################################################################################################################
 # Perform experiment.
 ########################################################################################################################
-# Initialize result arrays.
-total_number_of_operations_fast = np.zeros(number_of_nodes, dtype=np.int64)
-total_execution_time_fast = np.zeros(number_of_nodes, dtype=np.float64)
+# profiler_stats = similarity_slice_benchmark(number_of_trials=NUMBER_OF_TRIALS,
+#                                             adjacency_matrix=adjacency_matrix,
+#                                             rho_effective=RHO_EFFECTIVE,
+#                                             epsilon=EPSILON,
+#                                             laziness_factor=LAZINESS,
+#                                             profile_individual_stat_folder=profile_individual_stat_folder)
 
-total_number_of_operations_lazy = np.zeros(number_of_nodes, dtype=np.int64)
-total_execution_time_lazy = np.zeros(number_of_nodes, dtype=np.float64)
+# aggregate_measurements(number_of_trials=NUMBER_OF_TRIALS,
+#                        adjacency_matrix=adjacency_matrix,
+#                        profile_individual_stat_folder=profile_individual_stat_folder,
+#                        profile_aggregate_stat_folder=profile_aggregate_stat_folder)
 
-total_number_of_operations_rct = np.zeros(number_of_nodes, dtype=np.int64)
-total_execution_time_rct = np.zeros(number_of_nodes, dtype=np.float64)
-
-# Run trials.
-for trial in np.arange(NUMBER_OF_TRIALS):
-    number_of_operations_fast, execution_time_fast,\
-    number_of_operations_lazy, execution_time_lazy,\
-    number_of_operations_rct, execution_time_rct, = similarity_slice_benchmark(adjacency_matrix,
-                                                                               ALPHA,
-                                                                               EPSILON,
-                                                                               LAZINESS)
-
-    total_number_of_operations_fast += number_of_operations_fast
-    total_execution_time_fast += execution_time_fast
-
-    total_number_of_operations_lazy += number_of_operations_lazy
-    total_execution_time_lazy += execution_time_lazy
-
-    total_number_of_operations_rct += number_of_operations_rct
-    total_execution_time_rct += execution_time_rct
-
-########################################################################################################################
-# Print results.
-########################################################################################################################
-# Print Fast PageRank results.
-print("Fast PageRank:")
-print("Total nops: ", np.sum(total_number_of_operations_fast)/NUMBER_OF_TRIALS)
-print("Total time: ", np.sum(total_execution_time_fast)/NUMBER_OF_TRIALS)
-
-print(" Mean nops: ", np.mean(total_number_of_operations_fast)/NUMBER_OF_TRIALS)
-print(" Mean time: ", np.mean(total_execution_time_fast)/NUMBER_OF_TRIALS)
-
-# Print Lazy PageRank results.
-print("Lazy PageRank:")
-print("Total nops: ", np.sum(total_number_of_operations_lazy)/NUMBER_OF_TRIALS)
-print("Total time: ", np.sum(total_execution_time_lazy)/NUMBER_OF_TRIALS)
-
-print(" Mean nops: ", np.mean(total_number_of_operations_lazy)/NUMBER_OF_TRIALS)
-print(" Mean time: ", np.mean(total_execution_time_lazy)/NUMBER_OF_TRIALS)
-
-# Print Fast Regularized Commute-time (Cumulative Absorbing Random Walk) results.
-print("Fast RCT:")
-print("Total nops: ", np.sum(total_number_of_operations_rct)/NUMBER_OF_TRIALS)
-print("Total time: ", np.sum(total_execution_time_rct)/NUMBER_OF_TRIALS)
-
-print(" Mean nops: ", np.mean(total_number_of_operations_rct)/NUMBER_OF_TRIALS)
-print(" Mean time: ", np.mean(total_execution_time_rct)/NUMBER_OF_TRIALS)
+print_stats(number_of_trials=NUMBER_OF_TRIALS,
+            profile_aggregate_stat_folder=profile_aggregate_stat_folder,
+            profile_output_stat_folder=profile_output_stat_folder)
